6e0c6f0a8d4ad791c87cb01d3427280a789c753d
ArthurMoli <arthur_oliveira1@estudante.sc.senai.br>
2022-07-13 10:05:31 -0300

    .

--- src/main/java/com/Maci/service/ServicosServiceImpl.java
+++ src/main/java/com/Maci/service/ServicosServiceImpl.java
@@ -1,4 +1,16 @@
 package com.Maci.service;
 
-public class ServicosServiceImpl {
+import com.Maci.model.Usuario;
+import com.Maci.repository.ServicosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServicosServiceImpl implements ServicosService{
+    @Autowired
+    ServicosRepository servicosRepository;
+    @Override
+    public List<ServicosService> findAll() {return servicosRepository.findAll();}
 }
